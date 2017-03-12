f60829352282e2a37120e38d300b500abac62412
Saji Bhaskaran <sajibhaskaran@outlook.com>
2017-03-12 15:25:56 -0700

    initial commit

---
+++ WebProject/Context/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebProject.Models;
+
+namespace WebProject.Context
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+    }
+}
---
+++ WebProject/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
